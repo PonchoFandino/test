1829337a68b8b24cdffd1387880f59dc877bf570
Poncho <alfonsofandino@hotmail.com>
2018-10-29 17:10:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import me from './image/me.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div  className="preCenter">
+        <div className="center">
+          <img src={me} className="foto" alt="foto" />
+          <br/>
+          <h1>Alfonso Fandiño</h1>
+          <p>Bogota, Colombia.</p>
+        </div>
       </div>
     );
   }
